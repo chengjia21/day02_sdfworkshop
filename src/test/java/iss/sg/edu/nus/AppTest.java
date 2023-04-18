c23aaaa401c86adb7cf62f2033dc98d79d30a54f
chengjia21 <ccj.pepe@protonmail.com>
2023-04-18 15:24:43 +0800

    First commit

---
+++ src/test/java/iss/sg/edu/nus/AppTest.java
@@ -0,0 +1,20 @@
+package iss.sg.edu.nus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
